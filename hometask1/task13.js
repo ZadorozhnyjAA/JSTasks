0893718f5252834329624b8e3fae9022bc9e5315
Alexander Zadorozhnyj <od081181zaa@gmail.com>
2022-01-18 21:20:32 +0200

    1

--- hometask1/task13.js
+++ hometask1/task13.js
@@ -1,5 +1,5 @@
 var arr = [1, 2, 5, 9, 4, 13, 4, 10];
-for (var i = 0, n = 0; i < arr.length; i++) {
+for (var i = 0; i < arr.length; i++) {
     if (arr[i] == 4) {
         console.log ('Есть!');
         break;
