3edc1c3c3a1cdcfe0f592eb0d606946440196ad8
Alexander Zadorozhnyj <od081181zaa@gmail.com>
2022-01-18 21:18:58 +0200

    1

--- hometask1/task12.js
+++ hometask1/task12.js
@@ -1,5 +1,5 @@
 var arr = [1, 2, 3, -5, -2, 1, -4];
 for (var i = 0, sum = 0; i < arr.length; i++) {
-    if ( (arr[i] > 0) ) sum += arr[i];
+    if (arr[i] > 0) sum += arr[i];
 }
 console.log(sum);
