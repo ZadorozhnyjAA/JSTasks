44691837b7717a06b2814fc184da71a80a0c1766
Alexander Zadorozhnyj <od081181zaa@gmail.com>
2022-01-21 00:48:49 +0200

    1

--- hometask2/task2_3.js
+++ hometask2/task2_3.js
@@ -1,14 +1,11 @@
 function f(x,y,z) {
     var b = 0;
-    if (z === 0) { throw new Error ( 'Error: z = 0' ) } else {
+    if (z == 0) { throw new Error ( 'Error: z = 0' ) } else {
         for (var i = 0; i < arguments.length; i++) {
-                if ( typeof (arguments[i]) !=== 'number') {
-                    throw new Error ( 'Error: parametr type is not a number' ) } else {
-                        b = (x-y)/z;  
-                    }
+            if ( typeof (arguments[i]) !== 'number') {
+                throw new Error ( 'Error: parametr type is not a number' ) } else {b=(x-y)/z;}
         }
+        return b;
     }
-    return b;
-
 }
-console.log( f(2,4,9) );
+console.log( f(8,4,0) )
