72ffcd24cebff0fba864cff5fe7e8870e407ce9f
Alexander Zadorozhnyj <od081181zaa@gmail.com>
2022-01-22 01:24:38 +0200

    1

--- hometask2/task2_8.js
+++ hometask2/task2_8.js
@@ -1,13 +1,13 @@
-var arr = [1, 2, 3];
+var arr = [1, 2, 3, 4];
 function f (arr) {
     if (!Array.isArray(arr)) {throw new Error ( 'Error: parametr type shoul be an array' )};
     if (arr == 0) {throw new Error ( `Error: parametr can't be an empty` )};
-    var i = arr.length-1;
+    var i = arr.length - 1;
     function rec (arr) {        
-        if (i>0) {
+        if (i > 0) {
             console.log(arr[i]); 
-            
-            rec(arr[i-1]);
+            --i;
+            rec(arr);
         }
     }
     rec(arr);
