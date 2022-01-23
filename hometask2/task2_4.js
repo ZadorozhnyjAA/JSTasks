4e5f3f1e5c60f83254504beef92f9361fb0e0c01
Alexander Zadorozhnyj <od081181zaa@gmail.com>
2022-01-24 00:24:02 +0200

    1

--- hometask2/task2_4.js
+++ hometask2/task2_4.js
@@ -1,33 +1,11 @@
 function f(x) {   
-    function ChekArgInRange(){
-        if ( typeof (x) !== 'number') {throw new Error ( 'Error: parametr type is not a number' )} else 
-            if ( (x > 7) || (x<1) ) {throw new Error ( 'Error: parametr should be in range of 1 to 8' )}
-        
-    }
+  function ChekArgInRange(){
+      if ( typeof (x) !== 'number') {throw new Error ( 'Error: parametr type is not a number' )} else 
+          if ( (x > 7) || (x<1) ) {throw new Error ( 'Error: parametr should be in range of 1 to 8' )}
+      
+  }
 ChekArgInRange(x);                                         //Проверка аргументов - числа и в диапазоне или нет
-    
-switch (x) {
-    case 1:
-        console.log( 'Понедельник' );
-      break;
-    case 2:
-        console.log( 'Вторник' );
-      break;
-    case 3:
-        console.log( 'Среда' );
-      break;
-    case 4:
-        console.log( 'Четверг' );
-      break;
-    case 5:
-        console.log( 'Пятница' );
-      break;
-    case 6:
-        console.log( 'Суббота' );
-      break;
-    case 7:
-        console.log( 'Воскресенье' );
-      break;
+var week = ['Понедельник', 'Вторник', 'Среда','Четверг', 'Пятница', 'Суббота', 'Воскресенье'];    
+return week[x-1];
 }
-}
-console.log( f(5) );
+console.log( f(6) );
