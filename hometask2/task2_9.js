9ca429a88f77fbf982e5c1937de65806170292e4
Alexander Zadorozhnyj <od081181zaa@gmail.com>
2022-01-24 23:57:08 +0200

    1

--- hometask2/task2_9.js
+++ hometask2/task2_9.js
@@ -1,33 +1,27 @@
-function f(x) {   
-    function ChekArgInRange(x){
-        if ( typeof (x) !== 'number') {throw new Error ( 'Error: parametr type is not a number' )} else 
-            if ( (x > 7) || (x < 1) ) {throw new Error ( 'Error: parametr should be in range of 1 to 8' )}
-        
-    }
-ChekArgInRange(x);                                         //Проверка аргументов - числа и в диапазоне или нет
+function solution(x,y) {
+  var res = [];
+  var l = [];
+  var m = [];
+  var j = 0;
+  if (x.length > y.length) {
+    j = x.length;
+    k = x.length - y.length;
+    l = x;
+    m = y;
+  } else {
+    j = y.length;
+    k = y.length - x.length;
+    l = y;
+    m = x;
+  };
+  for (; j >= 0; j--){
+    z[j] = n + l[j] + k[j];
     
-switch (x) {
-    case 1:
-        console.log( 'Понедельник' );
-      break;
-    case 2:
-        console.log( 'Вторник' );
-      break;
-    case 3:
-        console.log( 'Среда' );
-      break;
-    case 4:
-        console.log( 'Четверг' );
-      break;
-    case 5:
-        console.log( 'Пятница' );
-      break;
-    case 6:
-        console.log( 'Суббота' );
-      break;
-    case 7:
-        console.log( 'Воскресенье' );
-      break;
+  }
+return res; 
 }
-}
-console.log(f(5));
+
+var x = [2, 4, 3];
+var y = [5, 6, 4];
+var z=Number(x[5]) + 4;
+console.log(z);
