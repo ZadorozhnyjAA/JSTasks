883fbd9b81fce24c4a0f007017e7ec517febf01b
Alexander Zadorozhnyj <od081181zaa@gmail.com>
2022-01-25 22:49:46 +0200

    1

--- hometask2/task2_9.js
+++ hometask2/task2_9.js
@@ -1,27 +1,23 @@
-function solution(x,y) {
-  var res = [];
-  var l = [];
-  var m = [];
-  var j = 0;
-  if (x.length > y.length) {
-    j = x.length;
-    k = x.length - y.length;
-    l = x;
-    m = y;
-  } else {
-    j = y.length;
-    k = y.length - x.length;
-    l = y;
-    m = x;
-  };
-  for (; j >= 0; j--){
-    z[j] = n + l[j] + k[j];
-    
+function replaceUndefined(q){
+  if (typeof(q) == 'undefined') {
+      q = null;
+      return q;} else {return q;}
   }
-return res; 
-}
-
-var x = [2, 4, 3];
-var y = [5, 6, 4];
-var z=Number(x[5]) + 4;
-console.log(z);
+  
+  function solution(x,y) {
+      let res = [];
+      let j = 0;
+      let n = 0;
+      if (x.length > y.length) { 
+          j = x.length - 1} else {j = y.length - 1};
+          for (; j >= 0; j--){
+              res[j] = n + replaceUndefined(x[j]) + replaceUndefined(y[j]);
+              if (res[j] >= 10) {n = 1; res[j] = res[j] - 10} else n=0;
+           
+      }
+      return res; 
+  }
+    
+  let x = [];
+  let y = [];
+  console.log(solution(x,y));
