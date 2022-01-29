c06eb81f5a30f73de09a62b24f4b742724f9e88a
Alexander Zadorozhnyj <od081181zaa@gmail.com>
2022-01-30 00:50:03 +0200

    1

--- Hometask3/task3_1.js
+++ Hometask3/task3_1.js
@@ -1,13 +1,16 @@
+"use strict";
 const arr = [1, 2, 3];
 function foreach ( a, f ) {
     if ((typeof(a) == "undefined") || (typeof(f) == "undefined")) {throw new Error ( `Error: parametr can't be an empty` )};
     if (!Array.isArray(a)) {throw new Error ( 'Error: it is not an array ' )};
     if (typeof(f) !== "function") {throw new Error ( 'Error: it is not an function ' )};    
-    var item;
-    for (var i = 0; i++; i < a.length) {
-        f(a[i]);
+    
+    for (var i = 0; i < a.length; i++) {
+        arguments[0] = a[i];
+        arguments[1]=i;
+        f(a[i], i, a);
     }
 }
-var b = (item, i, a) => {
-    console.log(`${item} имеет позицию ${i} в ${a}`)};
-foreach(arr, b);
+foreach(arr, (item, index, array) => {
+    console.log(`${item} имеет позицию ${index} в ${array}`)
+})
