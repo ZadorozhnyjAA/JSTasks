480cce91f30aa533d985a8e3427b076eec6c861a
Alexander Zadorozhnyj <od081181zaa@gmail.com>
2022-01-30 23:54:03 +0200

    1

---
+++ Hometask3/task3_8.js
@@ -0,0 +1,25 @@
+"use strict";
+function f ( arr ) {
+    if (!Array.isArray(arr)) {throw new Error ( 'Error: it is not an array ' )};
+    if ((typeof(arr) == "undefined") || (arr.length === 0)) {throw new Error ( `Error: parametr can't be an empty or empty array` )};
+    let resArr = [], res;
+    resArr = arr.flat(Infinity);
+    console.log(resArr);
+    for (let i = 0 ; i < resArr.length; i++) {
+        res += resArr[i];
+        console.log(resArr[i]);
+    }
+return res;
+
+}
+
+const arr = [[[1,2],[1,2]],[[2,1],[1,2]]];
+console.log(f(arr));
+const arr2 = [[[[[1,2]]]]];
+console.log(f(arr2));
+const arr3 = [[[[[1,2]]],2],1];
+console.log(f(arr3));
+const arr4 = [[[[[]]]]];
+console.log(f(arr4));
+const arr5 = [[[[[],3]]]];
+console.log(f(arr5));
