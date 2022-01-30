0dccb753d96a83bf6e926339f172880db6466745
Alexander Zadorozhnyj <od081181zaa@gmail.com>
2022-01-30 15:52:54 +0200

    1

---
+++ Hometask3/task3_3.js
@@ -0,0 +1,15 @@
+"use strict";
+const arr = [1, 2, 3];
+function every ( arr, f ) {
+    if ((typeof(arr) == "undefined") || (typeof(f) == "undefined")) {throw new Error ( `Error: parametr can't be an empty` )};
+    if (!Array.isArray(arr)) {throw new Error ( 'Error: it is not an array ' )};
+    if (typeof(f) !== "function") {throw new Error ( 'Error: it is not a function ' )};    
+    for (var i = 0; i < arr.length; i++) {
+        if (!f(arr[i], i, arr)) {                    
+            return false;
+        }    
+    }
+    return true;
+}
+
+console.log(every(arr, (item, i, arr) => item < 4));
