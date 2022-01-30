582e275e61b5ea990c986ba3f7266fb2396f2948
Alexander Zadorozhnyj <od081181zaa@gmail.com>
2022-01-30 23:17:49 +0200

    1

--- Hometask3/task3_10.js
+++ Hometask3/task3_10.js
@@ -2,7 +2,7 @@
 const arr = [3, 2, 1];
 function reverse ( arr ) {
     if (!Array.isArray(arr)) {throw new Error ( 'Error: it is not an array ' )};
-    if (arr.length === 0) {throw new Error ( `Error: parametr can't be an empty` )};
+    if ((typeof(arr) == "undefined") || (arr.length === 0)) {throw new Error ( `Error: parametr can't be an empty or empty array` )};
     let res = [];
     let n = 0;
     for (let i = (arr.length-1); i >= 0; i--) {
